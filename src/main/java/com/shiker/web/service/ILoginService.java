033eb66b49b824a730911a68e60870d4200c027d
guohaifeng <guohaifeng@baidu.com>
2016-06-23 16:23:05 +0800

    First commit

---
+++ src/main/java/com/shiker/web/service/ILoginService.java
@@ -0,0 +1,7 @@
+package com.shiker.web.service;
+
+/**
+ * Created by guohaifeng on 2016/6/23.
+ */
+public interface ILoginService {
+}
---
+++ src/main/java/com/shiker/web/service/impl/LoginService.java
@@ -0,0 +1,9 @@
+package com.shiker.web.service.impl;
+
+import com.shiker.web.service.ILoginService;
+
+/**
+ * Created by guohaifeng on 2016/6/23.
+ */
+public class LoginService implements ILoginService {
+}
